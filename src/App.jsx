4ba11521b2fe4d0c0525518c9a04247d61713173
Sandeep Kaur <sandeep.ss0903@gmail.com>
2025-02-21 15:07:17 -0500

    integrated reset password page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import ResetPassword from "./components/Auth/ResetPassword";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register/>} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       
       </Routes>
     </Router>
